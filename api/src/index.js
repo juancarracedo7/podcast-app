a8d0aad53aa9c1ca86b1d54d8a291619aaec084c
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2023-06-15 19:21:53 -0300

    formating and cleaning code

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const db = require('./database/index');
-const Podcast = require('../src/models/podcast');
-const PodcastDetail = require('./models/podcastDetails');
+const cors = require("cors");
+const db = require("./database/index");
+const Podcast = require("../src/models/podcast");
+const PodcastDetail = require("./models/podcastDetails");
 
 // Middleware
 app.use(cors());
@@ -11,28 +11,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Rutas
-app.use(require('./routes/index'));
+app.use(require("./routes/index"));
 
 // Sincronizar modelo con la base de datos
 
-
 const connect = async () => {
-    await Podcast.sync();
-    await PodcastDetail.sync();
-    console.log('Podcast table created');
-    };
+  await Podcast.sync();
+  await PodcastDetail.sync();
+  console.log("Podcast table created");
+};
 
 // Iniciar el servidor
 async function startServer() {
   try {
-    await db.sync({ force: true});
-    console.log('Conexión exitosa a la base de datos');
+    await db.sync({ force: true });
+    console.log("Conexión exitosa a la base de datos");
     await connect();
     app.listen(3001, () => {
-      console.log('Servidor en ejecución en el puerto 3001');
+      console.log("Servidor en ejecución en el puerto 3001");
     });
   } catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
+    console.error("Error al conectar a la base de datos:", error);
   }
 }
 
