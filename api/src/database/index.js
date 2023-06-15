60c24f09b86965af18d1d585dad315699b112119
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2023-06-15 15:08:30 -0300

    adding variable in .env

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const db = new Sequelize("podcast", "postgres", "postgres", {
-    host: "localhost",
-    dialect: "postgres",
-    port: 5432
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_DIALECT, DB_PORT } = process.env;
+
+
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+  port: DB_PORT,
 });
 
 module.exports = db;
